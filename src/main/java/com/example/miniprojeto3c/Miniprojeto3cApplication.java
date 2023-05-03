6f4ab0ae61bde160815c64648512579cd32debb2
berthiefieb <rm82925@estudante.fieb.edu.br>
2023-05-03 10:17:26 -0300

    primeiro commit

---
+++ src/main/java/com/example/miniprojeto3c/Miniprojeto3cApplication.java
@@ -0,0 +1,13 @@
+package com.example.miniprojeto3c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Miniprojeto3cApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Miniprojeto3cApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/miniprojeto3c/Miniprojeto3cApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miniprojeto3c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Miniprojeto3cApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
